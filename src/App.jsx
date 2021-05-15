c89cdd7a4d9ec41684f5e2f1059d756b7cc2b78e
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-05-15 15:33:53 +0900

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -15,14 +15,13 @@ const Header = () => {
   );
 }
 
-const Image = () => {
-  const url = "https://images.dog.ceo/breeds/shiba/shiba-8.jpg"
+const Image = (props) => { //Galleryの子コンポーネント
   return(
     <div className="card">
       <div className="card-image">
         <figure className="image">
           <img
-            src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg"
+            src={props.src} //{}の中には、Javascriptの式がかけるため変数だけでなく計算や関数呼び出しも可能
             alt="cute dog!"
           />
         </figure>
@@ -31,22 +30,44 @@ const Image = () => {
   );
 }
 
-const Gallery = () => {
+const Gallery = (props) => { //Mainの子コンポーネント  Mainからpropsを受け取る
+  const { urls } = props;//URLを一旦変数に格納
   return(
     <div className="columns is-vcentered is-multiline">
-      <div className="column is-3">
-        <Image />
-      </div>
+      {/* <div className="column is-3">
+        <Image src={url}/>
+      </div> */}
+      {urls.map((url) =>{ //URLの配列をmapメソッド  mapメソッドで作られるJSX式は最も外側に要素にkey属性をつけなけばいけません。
+        return( //urlの値をkeyとしている mapメソッドを使う場合はkey属性を必ずつける
+          <div key={url} className="column is-3"> 
+            <Image src={url} />
+          </div>
+        )
+      })}
     </div>
   );
 }
 
-const Main = () => {
+const Main = () => { //親コンポーネント
+  const urls = [ //この配列に含まれるURLそれぞれをImageコンポーネントに割り当てないといけない
+    "https://images.dog.ceo/breeds/shiba/shiba-11.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-12.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-14.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-17.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-2.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-3i.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-4.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-5.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-6.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-7.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-8.jpg",
+    "https://images.dog.ceo/breeds/shiba/shiba-9.jpg",
+  ];
   return(
     <main>
       <section className="section">
         <div className="container">
-          <Gallery />
+          <Gallery urls={urls}/>
         </div>
       </section>
     </main>
